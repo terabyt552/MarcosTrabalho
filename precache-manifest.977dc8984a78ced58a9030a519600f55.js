2d653a4f7dfd885d1cc6d4e399e811980dea9e32
Lucas Pereira Trindade <63750022+terabyt552@users.noreply.github.com>
2020-04-16 04:04:57 -0300

    Updates

---
+++ precache-manifest.977dc8984a78ced58a9030a519600f55.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53bd8b616da3d5efda9f3f6f57cbc5b6",
+    "url": "/MarcosTrabalho/index.html"
+  },
+  {
+    "revision": "fc68542765845355a9f8",
+    "url": "/MarcosTrabalho/static/css/2.1428c349.chunk.css"
+  },
+  {
+    "revision": "cfdf957971b657db78ee",
+    "url": "/MarcosTrabalho/static/css/main.e0831ff6.chunk.css"
+  },
+  {
+    "revision": "fc68542765845355a9f8",
+    "url": "/MarcosTrabalho/static/js/2.f6154501.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/MarcosTrabalho/static/js/2.f6154501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdf957971b657db78ee",
+    "url": "/MarcosTrabalho/static/js/main.a175a0d0.chunk.js"
+  },
+  {
+    "revision": "f43e7808e7ed3f5172f9",
+    "url": "/MarcosTrabalho/static/js/runtime-main.6a49501d.js"
+  },
+  {
+    "revision": "9cddf979e5e6b993873338e2bc96929c",
+    "url": "/MarcosTrabalho/static/media/CORBEL.9cddf979.TTF"
+  },
+  {
+    "revision": "c87f06ae40c78321844f2eac8b51737f",
+    "url": "/MarcosTrabalho/static/media/CORBELB.c87f06ae.TTF"
+  },
+  {
+    "revision": "d60144c3ff2e7aa33a232e42289d8c61",
+    "url": "/MarcosTrabalho/static/media/Corbel Bold Italic.d60144c3.ttf"
+  },
+  {
+    "revision": "7edf0c6793a64c1fef8d8844cb53bfdb",
+    "url": "/MarcosTrabalho/static/media/Corbel Bold.7edf0c67.ttf"
+  },
+  {
+    "revision": "b1ff3bddab5b75f1cb096b427a9e50f8",
+    "url": "/MarcosTrabalho/static/media/Corbel Italic.b1ff3bdd.ttf"
+  },
+  {
+    "revision": "43bf4ced5ab36415418ab37671eb88e1",
+    "url": "/MarcosTrabalho/static/media/LogoPesqAnimaçao.43bf4ced.png"
+  },
+  {
+    "revision": "57cf190008d1b34737fbe2acceb92299",
+    "url": "/MarcosTrabalho/static/media/LogoProgramador.57cf1900.png"
+  },
+  {
+    "revision": "c9994447ec2d3d1083ac4d15c16f1438",
+    "url": "/MarcosTrabalho/static/media/LupaPesquisar.c9994447.png"
+  },
+  {
+    "revision": "fb027b0be670bd551ecde47d43f7fc1b",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplay-Bold.fb027b0b.woff"
+  },
+  {
+    "revision": "b3907e5e1da9c62478f1d9aaaf5528a0",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplay-BoldItalic.b3907e5e.woff"
+  },
+  {
+    "revision": "6ef6f4fe86daaf841583907291d1e86a",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplay-Italic.6ef6f4fe.woff"
+  },
+  {
+    "revision": "107a6813ea92a7143e84c7fd5c3d65c4",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplay-Regular.107a6813.woff"
+  },
+  {
+    "revision": "a9a76d5d589e1d5da12769dbd74b4d12",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplaySC-Bold.a9a76d5d.woff"
+  },
+  {
+    "revision": "fd6837ba9441f8c469392995724286fd",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplaySC-BoldItalic.fd6837ba.woff"
+  },
+  {
+    "revision": "5b6f58814a7c1baf4a319d3eefc7b39d",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplaySC-Italic.5b6f5881.woff"
+  },
+  {
+    "revision": "7d3f7e4dc76c9b2b3882ab25db7f29b9",
+    "url": "/MarcosTrabalho/static/media/PlayfairDisplaySC-Regular.7d3f7e4d.woff"
+  },
+  {
+    "revision": "32169297a1c4d73ebf51803b74897891",
+    "url": "/MarcosTrabalho/static/media/ProgramadorLucas.32169297.png"
+  },
+  {
+    "revision": "4c4f1ead4fc423744421f15567ccf994",
+    "url": "/MarcosTrabalho/static/media/ProgramadorSostenes.4c4f1ead.png"
+  },
+  {
+    "revision": "21c892a7f38e340a2c73f61f1bb86c34",
+    "url": "/MarcosTrabalho/static/media/Rounded_Elegance.21c892a7.ttf"
+  },
+  {
+    "revision": "05d5de40c115cf97d776957293c2d255",
+    "url": "/MarcosTrabalho/static/media/corbeli.05d5de40.ttf"
+  },
+  {
+    "revision": "6c5e3e581b522258be508b4d8868025d",
+    "url": "/MarcosTrabalho/static/media/corbelz.6c5e3e58.ttf"
+  },
+  {
+    "revision": "60baf7fef9f797948db6fc0951c46000",
+    "url": "/MarcosTrabalho/static/media/e061552b0bc6615f8504893bc8c6fbb3.60baf7fe.gif"
+  },
+  {
+    "revision": "8ab23b9598e519f87896657c23a742af",
+    "url": "/MarcosTrabalho/static/media/slide1.8ab23b95.png"
+  },
+  {
+    "revision": "d91fb36a050493c4741564f874bb5d18",
+    "url": "/MarcosTrabalho/static/media/slide2.d91fb36a.png"
+  },
+  {
+    "revision": "51a519e536a66c6bd3bc0393f6d3feb8",
+    "url": "/MarcosTrabalho/static/media/slide3.51a519e5.png"
+  }
+]);
